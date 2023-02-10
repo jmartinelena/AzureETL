cb38fd59f1e79d58e0679b064656cb5c7a08eb88
federicopfund <federicopfund@gmail.com>
2023-02-10 20:41:39 -0300

    Lee de nodo Hadoop y escribe en nodo hadoop, streaming de csv.

--- cicd-etl/job/tasks/streaming.py
+++ cicd-etl/job/tasks/streaming.py
@@ -1,32 +1,98 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import expr
+from pyspark.sql.functions import explode
+from pyspark.sql.functions import split
+from session.logger import Log4j
 
-class Streaming(_prepare_spark)
+def main(spark,patterns):
+    '''
+    Main Streming Script.
+        Input : param spark :> Spark instancia.
+        Output: return      :> None.
+    '''
+    for Iter in patterns:
+        match Iter:                                      
+            case "VentasInternet.csv":  
+                                    
+                def tail(filePathsInput, n=10):
+                      'Return the last n lines of a file'
+                    with open(filePathsInput) as f:
+                        return deque(f, n)
+                 
+                if len(sys.argv) == 0:
+                    # Blobs storage
+                    filePaths = "wasbs://" + blob_container \
+                                             + "@" + storage_name \
+                                             + f".blob.core.windows.net/{Iter}"
+                else:
+                    # Local
+                    filePths = f'file:/opt/spark/spark-warehouse/{Iter}'
+                    # Hadoop nodo
+                    filePathsInput = f'hdfs://127.0.0.1:9000/user/fede/input/{Iter}' 
+                    filePathsOutput = f'hdfs://127.0.0.1:9000/user/fede/output/{Iter}' 
+                    
+                    # Spark DataFrame Infiriendo el esquema y especificando que el archivo contiene encavezado,
+                                        
+                    socketDF.isStreaming()    # Returns True for DataFrames that have streaming sources
+                    raw_df = spark.readStream \
+                                    .format("csv") \
+                                    .option("path", filePathsInput) \
+                                    .option("maxFilesPerTrigger", 1) \
+                                    .load()
 
-    def tail(filename, n=10):
-        'Return the last n lines of a file'
-        with open(filename) as f:
-            return deque(f, n)
+                    explode_df = raw_df.selectExpr("InvoiceNumber", "CreatedTime", "StoreID", "PosID",
+                                   "CustomerType", "PaymentMethod", "DeliveryType", "DeliveryAddress.City",
+                                   "DeliveryAddress.State",
+                                   "DeliveryAddress.PinCode", "explode(InvoiceLineItems) as LineItem")
 
-    def moving_average(iterable, n=3):
-        # moving_average([40, 30, 50, 46, 39, 44]) --> 40.0 42.0 45.0 43.0
-        # https://en.wikipedia.org/wiki/Moving_average
-        it = iter(iterable)
-        d = deque(itertools.islice(it, n-1))
-        d.appendleft(0)
-        s = sum(d)
-        for elem in it:
-            s += elem - d.popleft()
-            d.append(elem)
-            yield s / n
+                    flattened_df = explode_df \
+                        .withColumn("ItemCode", expr("LineItem.ItemCode")) \
+                        .withColumn("ItemDescription", expr("LineItem.ItemDescription")) \
+                        .withColumn("ItemPrice", expr("LineItem.ItemPrice")) \
+                        .withColumn("ItemQty", expr("LineItem.ItemQty")) \
+                        .withColumn("TotalValue", expr("LineItem.TotalValue")) \
+                        .drop("LineItem")
 
+                    invoiceWriterQuery = flattened_df.writeStream \
+                        .format("csv") \
+                        .queryName("Flattened Invoice Writer") \
+                        .outputMode("append") \
+                        .option("path", filePathsOutput) \
+                        .option("checkpointLocation", "chk-point-dir") \
+                        .trigger(processingTime="1 minute") \
+                        .start()
 
-    def roundrobin(*iterables):
-        "roundrobin('ABC', 'D', 'EF') --> A D E B F C"
-        iterators = deque(map(iter, iterables))
-        while iterators:
-            try:
-                while True:
-                    yield next(iterators[0])
-                    iterators.rotate(-1)
-            except StopIteration:
-                # Remove an exhausted iterator.
-                iterators.popleft()
+                    socketDF.printSchema()
+
+                    # Read all the csv files written atomically in a directory
+                    userSchema = StructType().add("name", "string").add("age", "integer")
+
+                    csvDF = spark \
+                            .readStream \
+                            .option("sep", ";") \
+                            .schema(userSchema) \
+                            .csv(filePths)  # Equivalent to format("csv").load("/path/to/directory")
+                                    
+                    logger.info("Flattened Invoice Writer started")
+                    invoiceWriterQuery.awaitTermination()
+                continue    
+
+            case _:
+                raise ValueError("No se encuenta el Arcvhivo.")
+    return None
+
+if __name__ == "__main__":
+     
+    import dotenv
+    import sys
+    import os
+    spark = SparkSession \
+            .builder \
+            .appName("File Streaming Demo") \
+            .master("local[3]") \
+            .config("spark.streaming.stopGracefullyOnShutdown", "true") \
+            .config("spark.sql.streaming.schemaInference", "true") \
+            .getOrCreate()
+
+    logger = Log4j(spark)
+    main(spark,patterns)
